736ea299399d43bbac03786b7c5077700bd125d3
hugo <hugo.a.da.silva@gmail.com>
2024-08-12 20:40:29 -0300

    resultado-escolar

---
+++ src/ResultadoEscolar.java
@@ -0,0 +1,17 @@
+
+
+public class ResultadoEscolar {
+public static void main(String[] args) {
+    int nota = 7;
+
+    if (nota >= 7)
+    System.out.println("Aprovado");
+
+    //true ou false
+    else if (nota >= 5 && nota < 7)
+    System.out.println("Recuperação");
+
+    else 
+    System.out.println("Reprovado");
+}
+}
