35cea9b94565ba06a07ce0966d7de0b460f8fe9b
hugo <hugo.a.da.silva@gmail.com>
2024-08-13 20:07:17 -0300

    break-continue

--- src/BreakContinue.java
+++ src/BreakContinue.java
@@ -2,7 +2,8 @@ public class BreakContinue {
 public static void main(String[] args) {
     for(int numero=1; numero <= 5; numero ++){
         if(numero == 3)
-        continue;
+        //break ou continue
+        continue; 
 
         System.out.println(numero);
     }
