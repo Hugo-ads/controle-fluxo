11c50f7842bc35f56abd761749db861d2dc98e5b
hugo <hugo.a.da.silva@gmail.com>
2024-08-15 19:49:40 -0300

    AboutMe

--- src/AboutMe.java
+++ src/AboutMe.java
@@ -6,25 +6,25 @@ public class AboutMe {
 public static void main(String[] args) {
     try{
     //criando o ojeto Scanner
-    Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+    Scanner scan = new Scanner(System.in).useLocale(Locale.US);
 
     System.out.println("Digite seu nome: ");
-    String nome = scanner.next();
+    String nome = scan.next();
 
     System.out.println("Digite seu sobrenome: ");
-    String sobrenome = scanner.next();
+    String sobrenome = scan.next();
 
     System.out.println("Digite sua idade: ");
-    int idade = scanner.nextInt();
+    int idade = scan.nextInt();
 
     System.out.println("Digite sua altura: ");
-    double altura = scanner.nextDouble();
+    double altura = scan.nextDouble();
 
     System.out.println("");
     System.out.println("Olá, me chamo " + nome.toUpperCase() + " " + sobrenome.toUpperCase());
     System.out.println("Tenho " + idade + " anos");
     System.out.println("Minha altura é: " + altura + "m");
-    scanner.close();
+    scan.close();
 }
     catch(InputMismatchException e){
         System.err.println("Os campos idade e altura precisam ser numéricos");
